b61a84f24bd0b12a44cf3dc8199b58f93955ef0b
Vitalik <vitalik@gmail.com>
2020-10-12 18:43:29 +0300

    added fractional number creation and tooltip creation

--- app.js
+++ app.js
@@ -1,28 +1,27 @@
-var c = 0,sum = 0, flag = 0, fl= false,i = 1, j= 2,k = 0,l = 1, m= 2, count = 0
-function numButton() {
+var c = 0,sum = 0, flag = 0, fl= false
+function numsButton() {
     let num = document.querySelectorAll('.num');
     for (let i = 0; i < num.length; i++) {
         let nums = num[i].innerText
         //console.log(num[i])
         num[i].addEventListener('click', function () {
-           inputNum(nums)
+            CreateOperand(nums)
         })
     }
 }
 
-function inputNum(nums) {
+function CreateOperand(nums) {
     const maxSizeInput = 12
-    const arr = [3, 7, 11, 16, 20]
+    const arr = [3, 7, 11, 15, 19]
     let inputs = document.getElementById('input')
     let txt = document.getElementById('num').value
     if (txt.length <= inputs.size) {
         if (txt.length >= maxSizeInput) {
             inputs.setAttribute("style", "font-size: 1.7rem")
         }
-         txt = txt + nums
-        for(let number of arr){
-            if(txt.length == number)
-            {
+        txt = txt + nums
+        for (let number of arr) {
+            if (txt.length == number && fl == false) {
                 txt = txt + ' '
             }
         }
@@ -60,30 +59,46 @@ function inputNum(nums) {
         //     txt1[m] = txt2
         //     txt = txt1.join('')
         // }
-
-        //     if (nums == ',') {
-        //         fl = true
-        //         if (count == 0) {
-        //             if (inputs.value == 0) {
-        //                 inputs.value = 0 + ','
-        //                 document.getElementById('num').value = 0 + ','
-        //                 count++
-        //             } else {
-        //                 inputs.value = txt
-        //                 document.getElementById('num').value = txt
-        //                 count++
-        //             }
-        //         }
-        //     }else {
-        //             inputs.value = txt
-        //             document.getElementById('num').value = txt
-        //         }
-        // }
-
-        inputs.value = txt
-        document.getElementById('num').value = txt
+        if (nums == ',') {
+            if (fl == false) {
+                if (inputs.value == '' || inputs.value == 0) {
+                    inputs.value = 0 + ','
+                    document.getElementById('num').value = 0 + ','
+                }else {
+                    inputs.value = txt
+                    document.getElementById('num').value = txt
+                }
+                fl = true
+            }
+        } else {
+            inputs.value = txt
+            document.getElementById('num').value = txt
+        }
     }
 }
+function CreatePrompt(target, operand){
+    let prompt = document.querySelector('.span')
+    if(target == document.querySelector('#equally').innerText){
+        prompt.innerText = ''
+    }
+    if(target == document.querySelector('#interest').innerText ){
+        console.log('ff')
+        prompt.innerText += ' ' + operand
+    }else{
+        prompt.innerText += ' ' + operand + ' ' + target
+    }
+
+}
+
+function getOperand(){
+    let inputs = document.getElementById('input')
+    let c = inputs.value
+    document.getElementById('num').value = ''
+   // inputs.value = ''
+    fl = false
+    return c
+}
+
 
 function searchPressingFuncButton() {
     let func = document.querySelectorAll('.func');
@@ -95,62 +110,69 @@ function searchPressingFuncButton() {
 }
 function allFunctionsButtons(e){
     let inputs = document.getElementById('input')
-    let func = e.target
-    switch (func.innerText){
+    switch (e.currentTarget.innerText){
         case '+': flag = 1
-            c = oneOperand()
+            c = getOperand()
+            CreatePrompt(e.currentTarget.innerText,c)
             break
         case '−': flag = 2
-            c = oneOperand()
+            c = getOperand()
+            CreatePrompt(e.currentTarget.innerText,c)
             break
         case '÷': flag = 3
-            c = oneOperand()
+            c = getOperand()
+            CreatePrompt(e.currentTarget.innerText,c)
             break
         case'⨯': flag = 4
-            c = oneOperand()
+            c = getOperand()
+            CreatePrompt(e.currentTarget.innerText,c)
             break
         case'C': removeInput()
+            fl = false
             break
         case'CE': inputs.value = 0
+            fl = false
             document.getElementById('num').value = ''
             break
         case'DEL': delChar()
+            fl = false
             break
         case'±': signOperand()
             break
         case'%': percent()
+            c = getOperand()
+            CreatePrompt(e.currentTarget.innerText,c)
             break
         case'√': root()
+            CreatePrompt(e.currentTarget.innerText,c)
             break
         case'x²': exponent()
-            count++
-
+            CreatePrompt(e.currentTarget.innerText,c)
             break
         case'1/x': fraction()
+            CreatePrompt(e.currentTarget.innerText,c)
             break
-
-
-
        default: operators()
+           CreatePrompt(e.currentTarget.innerText,c)
     }
 }
 
 function operators() {
     let inputs = document.getElementById('input')
     if (flag == 1) {
-        let d = oneOperand()
+        let d = getOperand()
         addNumbers(c,d)
         flag = 0
     } else if (flag == 2) {
-        let d = oneOperand()
+        let d = getOperand()
         subNumbers(c,d)
         flag = 0
     } else if (flag == 3) {
-        let d = oneOperand()
+        let d = getOperand()
         delNumbers(c,d)
         flag = 0
     } else if (flag == 4) {
-        let d = oneOperand()
+        let d = getOperand()
         mulNumbers(c,d)
         flag = 0
     }
@@ -159,7 +181,7 @@ function operators() {
 
 function fraction(){
     let inputs = document.getElementById('input')
-    let a = oneOperand()
+    let a = getOperand()
     let per = 1 / a
     inputs.value = per
     document.getElementById('num').value = ''
@@ -167,7 +189,7 @@ function fraction(){
 
 function root(){
     let inputs = document.getElementById('input')
-    let a = oneOperand()
+    let a = getOperand()
     let per = Math.sqrt(a)
     inputs.value = per
     document.getElementById('num').value = ''
@@ -175,7 +197,7 @@ function root(){
 
 function exponent(){
     let inputs = document.getElementById('input')
-    let a = oneOperand()
+    let a = getOperand()
     let per = +a * +a
     // if(Number.isInteger(per) == false){
     //     if(count == 0){
@@ -206,7 +228,7 @@ function exponent(){
 
 function percent(){
     let inputs = document.getElementById('input')
-    let a = oneOperand()
+    let a = getOperand()
     let per = +a / 100
     inputs.value = per
     document.getElementById('num').value = ''
@@ -214,15 +236,7 @@ function percent(){
 
 function signOperand(){
     let inputs = document.getElementById('input')
-    inputs.value =  -inputs.value
-}
-
-function oneOperand(){
-    let inputs = document.getElementById('input')
-    let c = inputs.value
-    document.getElementById('num').value = ''
-    inputs.value = ''
-    return c
+    inputs.value = inputs.value
 }
 
 function delChar(){
@@ -240,8 +254,10 @@ function delChar(){
 
  function removeInput(){
      let inputs = document.getElementById('input')
+     let prompt = document.querySelector('.span')
      inputs.value = ""
      document.getElementById('num').value = ''
+     prompt.innerText = ''
 }
 
 function mulNumbers(a,b){
@@ -273,5 +289,5 @@ function addNumbers(a, b){
         document.getElementById('num').value = ''
 }
 
-numButton()
+numsButton()
 searchPressingFuncButton()
